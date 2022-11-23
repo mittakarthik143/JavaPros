64130193b3d2f84c618ec525611d92def9c12582
mittakarthik143 <39511207+mittakarthik143@users.noreply.github.com>
2022-11-23 16:37:08 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,22 @@
+'''import re
+x=open('rough.py','r')
+y=re.findall(r'\w',x)
+print(y)'''
+
+"""ar =input('enter a list:')
+sum=0;
+for x in ar:
+    sum=sum+x;
+print("The Sum=",sum)"""
+
+"""print("begin")
+x=input("enter positive no")
+i=int(x)
+if i<10:
+    print("given no is 1 digit no")
+print("end")"""
+
+a=33
+b=400
+if b>a:
+    print("b is greater than a")
